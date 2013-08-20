686e287e5250699defa10ae1cfffe038c4bbb96f
kdurant <kdurant0725@163.com>
2013-08-20 12:55:08 +0800

    remove proj.m

--- key_expansion.m
+++ key_expansion.m
@@ -1,12 +1,12 @@
 function w = key_expansion (key, s_box, rcon)
     w = (reshape (key, 4, 4))';
     for i = 5 : 44
-        temp = w(i - 1, :);
+        temp = w(i - 1, :);    %get data of all line in array
         if mod (i, 4) == 1
             temp = rot_word (temp);
             temp = sub_bytes (temp, s_box);
             r = rcon ((i - 1)/4, :);
             temp = bitxor (temp, r);
         end
-        w(i, :) = bitxor (w(i - 4, :), temp);
+        w(i, :) = bitxor (w(i - 4, :), temp);       %join array
     end
